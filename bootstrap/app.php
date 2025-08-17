01af501146f25808e1f7446a3f7a79ff6d326c57
Musoftware <mahmoudmn810@gmail.com>
2025-08-17 14:58:31 +0300

    Remove RouteServiceProvider and update app bootstrap configuration to use AdminRouteServiceProvider, streamlining route management and enhancing application structure.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,7 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../console.php',
         health: '/up',
+        api: __DIR__.'/../routes/api.php',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         // Register custom middleware
@@ -26,7 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withProviders([
-        \App\Providers\RouteServiceProvider::class,
+        \App\Providers\AdminRouteServiceProvider::class,
     ])
     ->withExceptions(function (Exceptions $exceptions): void {
         //
